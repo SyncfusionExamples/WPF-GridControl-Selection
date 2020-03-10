b27f848bb9996b2741812db7db9c802d2c13b5f0
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2020-03-10 16:59:58 +0530

    Added sample for select the row or column and highlight the border for cell

---
+++ Select_row_column_usingcode/Select_row_column_usingcode/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Syncfusion.Windows.Controls.Grid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Select_row_column_usingcode
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            grid.Model.RowCount = 10;
+            grid.Model.ColumnCount = 8;
+            grid.Model.RowStyles[0].Borders.All = new Pen(Brushes.LightGray, 1);
+            grid.Model.ColStyles[0].Borders.All = new Pen(Brushes.LightGray, 1);
+
+            for(int i=0;i<10;i++)
+            {
+                for(int j=0;j<8;j++)
+                {
+                    grid.Model[i, j].CellValue = "R" + i + "C" + j;
+                }
+            }
+
+            //Programmatically select the row and column
+            grid.Model.Selections.Add(GridRangeInfo.Row(4));
+            grid.Model.Selections.Add(GridRangeInfo.Col(3));
+
+        }
+    }
+}
---
+++ Selection_cellborder_color/Change_bordercolor/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Change_bordercolor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            grid.Model.RowCount = 10;
+            grid.Model.ColumnCount = 7;
+            grid.Model.Options.ExcelLikeSelectionFrame = true;
+            grid.Model.HeaderStyle.Borders.All = new Pen(Brushes.LightGray, 1);
+            grid.Model.QueryCellInfo += Model_QueryCellInfo;            
+            grid.Model.Options.AllowSelection = Syncfusion.Windows.Controls.Grid.GridSelectionFlags.Row;
+            grid.Model.Options.HighlightSelectionBorder = Brushes.Green;
+        }
+
+        private void Model_QueryCellInfo(object sender, Syncfusion.Windows.Controls.Grid.GridQueryCellInfoEventArgs e)
+        {
+            for (int i = 0; i <= 3; i++)
+            {
+                if (e.Style.RowIndex == 0 && e.Style.ColumnIndex > 0)
+                    e.Style.CellValue = " Col " + e.Style.ColumnIndex;
+                else if (e.Style.ColumnIndex == 0 && e.Style.RowIndex > 0)
+                    e.Style.CellValue = " Row " + e.Style.RowIndex;
+                else if (e.Style.ColumnIndex == 0 && e.Style.RowIndex == 0)
+                    e.Style.CellValue = " Grid";
+            }
+        }
+    }
+}
