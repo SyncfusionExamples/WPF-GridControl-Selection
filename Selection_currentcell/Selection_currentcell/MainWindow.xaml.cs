037a69b2b356b5f0f9260189ac4d3b88d09b7a2c
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2020-03-23 10:37:07 +0530

    Added sample for change the current cell background

---
+++ Selection_currentcell/Selection_currentcell/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Syncfusion.Windows.Controls.Grid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Selection_currentcell
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            grid.Model.RowCount = 10;
+            grid.Model.ColumnCount = 7;
+            grid.Model.RowStyles[0].Borders.All = new Pen(Brushes.LightGray, 1);
+            grid.Model.ColStyles[0].Borders.All = new Pen(Brushes.LightGray, 1);
+            grid.Model.Options.AllowSelection = Syncfusion.Windows.Controls.Grid.GridSelectionFlags.Any;
+            grid.Model.Options.ActivateCurrentCellBehavior = Syncfusion.Windows.Controls.Grid.GridCellActivateAction.DblClickOnCell;
+            grid.Model.Options.ShowCurrentCell = false;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    grid.Model[i, j].CellValue = "R" + i + "C" + j;
+                }
+            }
+
+        }
+    }
+}
